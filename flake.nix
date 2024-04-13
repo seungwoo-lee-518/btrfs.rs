efddc6f3c86433f3f065adb670f31a709cef1565
SEUNGWOO LEE <seungwoo.lee.518@proton.me>
2024-04-13 10:06:51 +0000

    chore(flake.nix): nix flake init && add required packages

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.defeault = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
+        cargo
+        rustc
+        rustfmt
+        rust-analyzer
+
+        openssl
+        pkg-config
+      ];
+    };
+  };
+}
